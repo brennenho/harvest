ef7091ee1b26fd7e0a75d0d3026a3a1a911ada73
David Bai <142951611+dav1dbai@users.noreply.github.com>
2023-10-21 21:08:38 -0700

    added chatbot history

--- main.py
+++ main.py
@@ -1,26 +1,56 @@
-# import necessary packages
-import os
+import sys
+import time
+from decouple import config
+import json
 import cohere
+import random
+import string
 
-# initialize cohere api client
-co = cohere.Client('nTI4k8KctOt7AiB6JdKIJhnwDUeU1BzauNM6G6Op')
+CHAT_MODEL = 'command'
 
-# initialize a conversation session id
+co = cohere.Client(config('CO_API_KEY'))
 cohere_chat_res_start = co.chat("Hi")
-conv_session_id = cohere_chat_res_start.conversation_id 
 
-# continue existing chat session
 def talk(prompt):
-    response = co.generate(  
-        model='command-nightly',  
-        prompt = prompt,  
-        temperature=0.750)
+    response = co.chat(  
+        model='command',  
+        message = prompt,  
+        temperature=0.3,
+        chat_history = history,
+        # stream = True,
+        prompt_truncation = 'auto',
+        citation_quality = 'accurate',
+        connectors=[{"id": "web-search"}]
+        )
+    return response
 
-    return response.generations[0].text
+history = []
 
-# take prompt from user and call talk function. run this in loop till user exits
 while True:
     prompt = input("You: ")
     if prompt == "exit":
         break
-    print("Bot: ", talk(prompt))
+
+    conv_session_id = cohere_chat_res_start.conversation_id
+    streaming_gens = talk(prompt)
+
+    history.append({'role': 'User', 'message':prompt})
+
+    print(streaming_gens.text)
+    history.append({'role': 'Chatbot', 'message':streaming_gens.text})
+    '''
+    for i, token in enumerate(streaming_gens):
+        event_type = token.event_type
+        if(event_type=='text-generation'):
+            print(f"{token.text}")
+        if(event_type=='stream-end'):
+            break
+    '''
+
+    for doc in streaming_gens.documents:
+        print(doc['title'])
+        print(doc['url'])
+
+
+
+

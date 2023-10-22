6b6fc58bcd009129966887f84249584457a4a2d8
David Bai <142951611+dav1dbai@users.noreply.github.com>
2023-10-22 00:31:59 -0700

    Flask API call for Cohere AI

---
+++ farmerapi/views.py
@@ -0,0 +1,62 @@
+from flask import Blueprint, request, jsonify
+from decouple import config
+import json
+import cohere
+ 
+co = cohere.Client(config('CO_API_KEY'))
+
+prompt_data = ""
+history = []
+
+CHAT_MODEL = 'command'
+def talk(prompt):
+    global history
+    response = co.chat(  
+        model='command',  
+        message = prompt,  
+        temperature=0.3,
+        chat_history = history,
+        # stream = True,
+        prompt_truncation = 'auto',
+        citation_quality = 'accurate',
+        connectors=[{"id": "web-search"}]
+    )
+    history.append({'role': 'User', 'message':prompt})
+    return response
+
+persona_assignment = '''
+    Your name is Farmer.AI. You are an LLM based chatbot that is supposed to answer questions
+      from farmers and hobbyists related to sustainable agricultural practices. You must provide
+     responses to questions asked by the user that include evidence of your claims. Also, identify
+     when the user is wishing to use unsustainable agricultural practices and instead suggest better
+    evidence based options. Most importantly, always explain your reasoning behind a decision step-by-step.
+     If a user talks about something they want to do, you must analyze that action for sustainability and
+       possible environmentally harmful consequences.
+       '''
+
+talk(persona_assignment)
+
+main = Blueprint('main', __name__)
+
+@main.route('/add_prompt', methods=['POST'])
+def add_prompt():
+    global prompt_data
+    prompt_data = request.get_json()['text']
+    return prompt_data
+
+@main.route('/response')
+def response():
+    # do the cohere API call here
+    global prompt_data
+    prompt = prompt_data
+    streaming_gens = talk(prompt)
+
+    json_object = {
+        "text": streaming_gens.text,
+        "documents": streaming_gens.documents
+    }
+
+    json_out = json.dumps(json_object)
+
+    return json_out
+    # return 
